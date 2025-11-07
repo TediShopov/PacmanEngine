568360d18dbacc20f808919a35186d26e30ff03d
TediShopov <tedikatana@abv.bg>
2025-11-07 10:03:15 +0000

    Pacman rotating to the direction of movement

--- PacmanEngine/Pacman.h
+++ PacmanEngine/Pacman.h
@@ -15,7 +15,20 @@ public:
 	bool canTraverse(GameLevelGrid::TileType type) const override
 	{
 		return type != GameLevelGrid::TileType::Door && type != GameLevelGrid::TileType::Wall;
+	}
 
+	void fixedUpdate(float dt) override
+	{
+		//Call based classes fixed update
+		GridEntity::fixedUpdate(dt);
+		//Additionally, rotate sprite to the direction of movement
+		rotateToDirection(sf::Vector2f(currentDirecton));
+	}
+
+	void rotateToDirection(sf::Vector2f dir) 
+	{
+		sf::Angle angle = sf::radians(std::atan2(dir.y,dir.x));
+		this->sprite->setRotation(angle + sf::degrees(180));
 	}
 
 
