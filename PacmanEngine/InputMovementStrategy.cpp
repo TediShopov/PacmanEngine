9c90aa730c3ac5183a50c5590ab7281b7f2d22c1
TediShopov <tedikatana@abv.bg>
2025-11-13 19:13:29 +0000

    Added arrow keys additional control scheme

--- PacmanEngine/InputMovementStrategy.cpp
+++ PacmanEngine/InputMovementStrategy.cpp
@@ -6,14 +6,15 @@ sf::Vector2i InputMovementStrategy::computeDesiredDirection(const GridEntity& se
 {
 	auto grid = self.levelGrid;
 	sf::Vector2i desiredDirection = self.getDesiredDirection();
-	if (input->isKeyDown((int)sf::Keyboard::Key::A))
+	if (input->isKeyDown((int)sf::Keyboard::Key::A) ||  input->isKeyDown((int)sf::Keyboard::Key::Left))
 		desiredDirection = GameLevelGrid::Directions.at(LEFT);
-	else if (input->isKeyDown((int)sf::Keyboard::Key::D))
+	else if (input->isKeyDown((int)sf::Keyboard::Key::D)||  input->isKeyDown((int)sf::Keyboard::Key::Right))
 		desiredDirection = GameLevelGrid::Directions.at(RIGHT);
-	else if (input->isKeyDown((int)sf::Keyboard::Key::W))
+	else if (input->isKeyDown((int)sf::Keyboard::Key::W)||  input->isKeyDown((int)sf::Keyboard::Key::Up))
 		desiredDirection = GameLevelGrid::Directions.at(UP);
-	else if (input->isKeyDown((int)sf::Keyboard::Key::S))
+	else if (input->isKeyDown((int)sf::Keyboard::Key::S)||  input->isKeyDown((int)sf::Keyboard::Key::Down))
 		desiredDirection = GameLevelGrid::Directions.at(DOWN);
+
 	return desiredDirection;
 }
 
